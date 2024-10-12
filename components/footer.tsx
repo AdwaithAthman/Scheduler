f214690cfa0b765889be834f42da88bcc1b9a400
AdwaithAthman <iamadwaith17@gmail.com>
2024-10-13 01:16:20 +0530

    Updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
+// import Link from "next/link";
+import { LinkPreview } from "@/components/ui/link-preview";
 
 export function Footer () {
   return (
     <div className="bg-background py-6">
         <div className="mx-auto container text-center">
-            Made with ❤️ by <Link href="https://twitter.com/0xSatoshi" target="_blank" rel="noreferrer" className="text-foreground hover:underline">Adwaith Athman</Link>
+            Made with ❤️ by <LinkPreview url="https://www.linkedin.com/in/adwaith-athman/" className="text-foreground ml-1 hover:underline">Adwaith Athman</LinkPreview>
         </div>
     </div>
   )
